1e82390c0d52feaf29880bc8778507dd96033c69
AdrianR25 <35237320+AdrianR25@users.noreply.github.com>
2026-05-13 18:12:45 +0200

    perf(landing): :zap: reduce unused CSS

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://labelier.adrianr.dev',
-  integrations: [sitemap()]
+  integrations: [sitemap(), purgecss()]
 });
